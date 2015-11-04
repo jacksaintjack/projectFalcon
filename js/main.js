0d5d4ccf32bb8cabde4b818461cc4437f62447af
Nicholas Glover <jacksaintjack@gmail.com>
2015-11-04 16:50:43 -0600

    added the views

--- js/main.js
+++ js/main.js
@@ -1,6 +1,49 @@
 
+///***************COMMENT OUT FOR LATER**********************//
+
+//Our views for our pages
+// var LoginView = Backgone.View.extend({
+//   template: _template(////$('#addtemplate here').html());
+//
+//   render: function(){
+//     this.$el.html(this.template());
+//   }
+// });
+
+// var RegistrationView = Backbone.View.extend({
+//   template: _template(////$('#template here').html())
+//
+//   render: function(){
+//     this.$el.html(this.template());
+//   }
+// });
+
+// var DashboardView = Backbone.View.extend({
+//   template: _template($("#addTemplateHere").html());
+//
+//   renter: function(){
+//     this.$el.html(this.template());
+//   };
+// });
+
+// var UsersView = Backbone.View.extend({
+//   template: _template($('#changeThis').html());
+//
+//   render: function(){
+//     this.$el.html(this.template());
+//   }
+// });
+
+// var UserIdView = Backbone.View.extend({
+//   template: _template($("#add Template").html());
+//
+//   render: function(){
+//     this.$el.html(this.template);
+//   }
+// });
+
 //Our Router for our
-var  FalconRouter = Backbone.Router.extend({
+var FalconRouter = Backbone.Router.extend({
 
   routes: {
     "": "home",
@@ -16,7 +59,7 @@ var  FalconRouter = Backbone.Router.extend({
   },
 
   loginRoute: function(){
-
+  
   },
 
   registrationRoute: function(){
@@ -37,4 +80,4 @@ var  FalconRouter = Backbone.Router.extend({
 
 
 
-})
+});

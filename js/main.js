e1c69fdb0bbcfaf0294806f9b6e1c6adfde03847
Nicholas Glover <jacksaintjack@gmail.com>
2015-11-05 08:34:02 -0600

    Added in the router a way for our templates to display using render

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 
 ///***************COMMENT OUT FOR LATER**********************//
 
+// var HomeView = Backbone.View.extend({
+//   template: _template($('#addTemplateHere').html());
+//
+//   router: function(){
+//     this.$el(this.template.html());
+//   }
+// })
+
 //Our views for our pages
 // var LoginView = Backgone.View.extend({
 //   template: _template(////$('#addtemplate here').html());
@@ -46,7 +54,7 @@
 var  FalconRouter = Backbone.Router.extend({
 
   routes: {
-    "": "home",
+    "home": "home",
     "login": "loginRoute",
     "registration": "registrationRoute",
     "dashboard": "dashboardRoute",
@@ -55,27 +63,40 @@ var  FalconRouter = Backbone.Router.extend({
   },
 
   home: function(){
-
+    // //To be used later to show the template views of the page
+    // var view = new HomeView()
+    // view.render(); // running the render function in HomeView
+    // $('#classOrIdgoeshere').html(view.$el); // Tells it where the template should be loaded under
   },
 
   loginRoute: function(){
-
+    // var view = new LoginView();
+    // view.render();
+    // $('#classOrIdgoeshere').html(view.$el);
   },
 
   registrationRoute: function(){
-
+    // var view = new RegistrationView();
+    // view.render();
+    // $('#classOrIdgoeshere').html(view.$el);
   },
 
   dashboard: function(){
-
+    // var view = new DashboardView();
+    // view.render();
+    // $('#classOrIdgoeshere').html(view.$el);
   },
 
   users: function(){
-
+    // var view = new UsersView();
+    // view.render();
+    // $('#classOrIdgoeshere').html(view.$el);
   },
 
   useridRoute: function(){
-
+    // var view = UserIdView();
+    // view.render();
+    // $('#classOrIdgoeshere').html(view.$el);
   }
 
 
